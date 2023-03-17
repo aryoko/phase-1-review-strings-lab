09854555e886af2e986beca112fa44cd90239fbb
Ary Okoli <aco.umeadi@gmail.com>
2023-03-17 16:56:05 -0400

    string lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Bilbo Baggins";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+// const excitedWelcomeMessage = "\'Welcome to Flatbook'.toUpperCase.(), {$currentUser}.toUpperCase() !";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let firstIniitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstIniitial} !`;
